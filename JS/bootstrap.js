fd7a718ef9153687ecaa7d7efe057d9825c34a8b
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2024-08-05 14:23:58 +0700

    nav themes

--- JS/bootstrap.js
+++ JS/bootstrap.js
@@ -13,19 +13,3 @@ window.onclick = function (event) {
     }
   }
 };
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("nav-dropdown-api");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
